1402eeb7accf0dc3ccc0ae41a0a7b4d90824b8e3
wlamirl <wlamirl@yahoo.com.br>
2023-07-24 13:13:01 -0300

    ~useEffect + useReducer + useContext - parte 2

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,11 +1,19 @@
 import { useContext, useEffect, useRef } from 'react';
 import { loadPosts } from '../../contexts/PostsProvider/actions';
 import { PostsContext } from '../../contexts/PostsProvider/context';
+import { CounterContext } from '../../contexts/CounterProvider/context';
+import {
+  decrementCounter,
+  incrementCounter,
+} from '../../contexts/CounterProvider/action';
 
 export const Posts = () => {
   const isMounted = useRef(true);
   const postsContext = useContext(PostsContext);
   const { postsState, postsDispatch } = postsContext;
+
+  const counterContext = useContext(CounterContext);
+  const { counterState, counterDispatch } = counterContext;
 
   useEffect(() => {
     loadPosts(postsDispatch).then((dispatch) => {
@@ -21,6 +29,12 @@ export const Posts = () => {
 
   return (
     <div>
+      <button onClick={() => incrementCounter(counterDispatch)}>
+        Counter {counterState.counter}+
+      </button>
+      <button onClick={() => decrementCounter(counterDispatch)}>
+        Counter {counterState.counter}-
+      </button>
       <h1>POSTS</h1>
       {postsState.loading && (
         <p>
